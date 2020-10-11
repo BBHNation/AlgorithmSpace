a445b83913a532879b84f3ea550101e0a696773d
Danie10ne <wanweixiang2017@163.com>
2020-10-11 18:21:49 +0800

    levelOrder

---
+++ src/main/java/com/algorithm/space/levelOrder.java
@@ -0,0 +1,42 @@
+package main.java.com.algorithm.space;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class levelOrder {
+
+     //Definition for a binary tree node.
+     public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+     }
+
+    public int[] levelOrder(TreeNode root) {
+        if(root == null){
+            return new int[0];
+        }
+        //使用队列从左到右遍历二叉树的每一层节点
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            res.add(node.val);
+            //如果有左子树，将左子树的节点加入队列
+            if(node.left != null){
+                queue.offer(node.left);
+            }
+            //如果有右子树，将右子树的节点加入队列
+            if(node.right != null){
+                queue.offer(node.right);
+            }
+        }
+        int[] result = new int[res.size()];
+        for(int i = 0; i < res.size(); i++){
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+}
