caf69f2ae85009e9a08d09870ad12d1bdf8ac231
BBHancock <bai.szsy@qq.com>
2020-10-19 12:43:43 +0800

    #Q22 - getKthFromEnd

---
+++ src/main/java/com/algorithm/space/hancock/solution/SolutionOf22.java
@@ -0,0 +1,23 @@
+package com.algorithm.space.hancock.solution;
+
+import com.algorithm.space.hancock.solution.common.ListNode;
+
+public class SolutionOf22 {
+  public ListNode getKthFromEnd(ListNode head, int k) {
+    ListNode fastPointer = head;
+    ListNode slowPointer = head;
+    for(int index = 0; index < k; index++) {
+      if (fastPointer == null) {
+        return null;
+      }
+      fastPointer = fastPointer.next;
+    }
+
+    while (fastPointer != null) {
+      fastPointer = fastPointer.next;
+      slowPointer = slowPointer.next;
+    }
+
+    return slowPointer;
+  }
+}
---
+++ src/test/java/com/algorithm/space/hancock/solution/SolutionOf22Test.java
@@ -0,0 +1,28 @@
+package com.algorithm.space.hancock.solution;
+
+import com.algorithm.space.hancock.solution.common.ListNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionOf22Test {
+  @Test
+  void getKthFromEnd_shouldReturn45_givenListIs12345Input2() {
+    // given
+    ListNode node1 = new ListNode(1);
+    ListNode node2 = new ListNode(2);
+    ListNode node3 = new ListNode(3);
+    ListNode node4 = new ListNode(4);
+    ListNode node5 = new ListNode(5);
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+
+    // when
+    ListNode kthFromEnd = new SolutionOf22().getKthFromEnd(node1, 2);
+
+    // then
+    Assertions.assertEquals(4, kthFromEnd.val);
+    Assertions.assertEquals(5, kthFromEnd.next.val);
+  }
+}
