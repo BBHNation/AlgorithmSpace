f30f452e6a5c13115b5462dc96cfeda63133865b
BBHancock <bai.szsy@qq.com>
2020-10-21 21:11:20 +0800

    #Q28 - isSymmetric

---
+++ src/main/java/com/algorithm/space/hancock/solution/SolutionOf28.java
@@ -0,0 +1,16 @@
+package com.algorithm.space.hancock.solution;
+
+import com.algorithm.space.hancock.solution.common.TreeNode;
+
+public class SolutionOf28 {
+  public boolean isSymmetric(TreeNode root) {
+    return isSymmetric(root, root);
+  }
+
+  public boolean isSymmetric(TreeNode leftRoot, TreeNode rightRoot) {
+    if (leftRoot == null && rightRoot == null) return true;
+    if (leftRoot == null || rightRoot == null || leftRoot.val != rightRoot.val) return false;
+    return isSymmetric(leftRoot.left, rightRoot.right) && isSymmetric(leftRoot.right, rightRoot.left);
+  }
+
+}
