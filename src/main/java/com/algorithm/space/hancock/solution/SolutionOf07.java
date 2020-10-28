f45219ed1809190d2f7982a146914678d1c37a13
BBHancock <bai.szsy@qq.com>
2020-10-28 12:53:19 +0800

    #Q39 - majorityElement

---
+++ src/main/java/com/algorithm/space/hancock/solution/SolutionOf07.java
@@ -0,0 +1,12 @@
+package com.algorithm.space.hancock.solution;
+
+import com.algorithm.space.hancock.solution.common.TreeNode;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class SolutionOf07 {
+
+  public TreeNode buildTree(int[] preorder, int[] inorder) {
+    throw new NotImplementedException();
+  }
+
+}
---
+++ src/main/java/com/algorithm/space/hancock/solution/SolutionOf32.java
@@ -0,0 +1,12 @@
+package com.algorithm.space.hancock.solution;
+
+import com.algorithm.space.hancock.solution.common.TreeNode;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+public class SolutionOf32 {
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    throw new NotImplementedException();
+  }
+}
