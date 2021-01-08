61e9f48300418e62269de21c9aa0f036cc0609d6
BBHNation <bai.szsy@qq.com>
2021-01-08 14:02:43 +0800

    #Q07 - build tree

--- src/main/java/com/algorithm/space/hancock/solution/SolutionOf07.java
+++ src/main/java/com/algorithm/space/hancock/solution/SolutionOf07.java
@@ -1,12 +1,46 @@
 package com.algorithm.space.hancock.solution;
 
-import com.algorithm.space.hancock.solution.common.NotImplementException;
+import com.algorithm.space.hancock.solution.common.NoticeException;
 import com.algorithm.space.hancock.solution.common.TreeNode;
+
+import java.util.Arrays;
 
 public class SolutionOf07 {
 
-  public TreeNode buildTree(int[] preorder, int[] inorder) {
-      throw new NotImplementException("Solution Of 07 not implemented.");
-  }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length < 1 || inorder.length < 1) {
+            return null;
+        } else if (preorder.length != inorder.length) {
+            return null;
+        } else if (preorder.length == 1 && inorder.length == 1 && preorder[0] == inorder[0]) {
+            return new TreeNode(preorder[0]);
+        } else {
+            TreeNode root = new TreeNode(preorder[0]);
+
+            int leftTreeSize = 0;
+            int rightTreeSize = 0;
+            for (int index = 0; index < inorder.length; index++) {
+                if (inorder[index] == preorder[0]) {
+                    leftTreeSize = index;
+                    rightTreeSize = inorder.length - index - 1;
+                    break;
+                }
+            }
+
+            if (leftTreeSize != 0) {
+                int[] leftPreOrder = Arrays.copyOfRange(preorder, 1, leftTreeSize+1);
+                int[] leftInOrder = Arrays.copyOfRange(inorder, 0, leftTreeSize);
+                root.left = buildTree(leftPreOrder, leftInOrder);
+            }
+
+            if (rightTreeSize != 0) {
+                int[] rightPreOrder = Arrays.copyOfRange(preorder, 1+leftTreeSize, 1+leftTreeSize+rightTreeSize);
+                int[] rightInOrder = Arrays.copyOfRange(inorder, leftTreeSize+1, inorder.length);
+                root.right = buildTree(rightPreOrder, rightInOrder);
+            }
+
+            return root;
+        }
+    }
 
 }
--- src/main/java/com/algorithm/space/hancock/solution/common/TreeNode.java
+++ src/main/java/com/algorithm/space/hancock/solution/common/TreeNode.java
@@ -1,9 +1,27 @@
 package com.algorithm.space.hancock.solution.common;
 
-public class TreeNode {
+import lombok.EqualsAndHashCode;
+import org.assertj.core.api.Condition;
+
+import java.util.Objects;
+
+public class TreeNode extends Condition<TreeNode> {
     public int val;
     public TreeNode left;
     public TreeNode right;
     public TreeNode(int x) { val = x; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TreeNode treeNode = (TreeNode) o;
+        return val == treeNode.val && Objects.equals(left, treeNode.left) && Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, left, right);
+    }
 }
 
